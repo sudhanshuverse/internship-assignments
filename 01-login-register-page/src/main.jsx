683e15125658820f207ef4ecceaf665f3725e752
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2026-02-10 17:41:31 +0530

    Update the code to live the site

--- 01-login-register-page/src/main.jsx
+++ 01-login-register-page/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-  </HashRouter>
-);
+  </BrowserRouter>
+)
