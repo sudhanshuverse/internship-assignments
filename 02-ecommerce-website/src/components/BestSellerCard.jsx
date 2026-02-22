1682ff1a916ddb103982f02cd3aa91769c91ffe2
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-02-22 12:19:32 +0530

    Update BestSellerCard.jsx code to solve the dynamic routing problem

--- 02-ecommerce-website/src/components/BestSellerCard.jsx
+++ 02-ecommerce-website/src/components/BestSellerCard.jsx
@@ -1,24 +1,14 @@
 const BestSellerCard = ({ product }) => {
     return (
-
-        <a
-            href={`/product/${product.id}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="product-link"
-        >
+        <a href={`/product/${product.id}`} className="product-link">
             <div className="items-card">
-
                 <div className="card-image-container">
                     <img src={product.image} alt={product.name} />
                 </div>
-
                 <p className="card-title">{product.name}</p>
                 <strong className="card-price">${product.price}</strong>
-
             </div>
         </a>
-
     );
 };
 
