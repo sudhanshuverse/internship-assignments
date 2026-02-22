7624db899dc36603ecee854323f4b496f650665f
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-02-22 12:10:27 +0530

    Updated Home.jsx file

--- 02-ecommerce-website/src/pages/Home.jsx
+++ 02-ecommerce-website/src/pages/Home.jsx
@@ -2,8 +2,7 @@ import './Home.css';
 import women from '../assets/images/women.png';
 import hero_first_icon from '../assets/icons/hero-first-icon.svg';
 import hero_second_icon from '../assets/icons/hero-second-icon.svg';
-import { products, best_seller } from '../data/products';
-import ProductCard from '../components/ProductCard';
+import { latest_products, home_best_seller } from '../data/products'; import ProductCard from '../components/ProductCard';
 import BestSellerCard from '../components/BestSellerCard';
 import exchange_policy_icon from '../assets/icons/exchange_policy_icon.svg'
 import return_policy_icon from '../assets/icons/return_policy_icon.svg';
@@ -45,7 +44,7 @@ const Home = () => {
                         <p className='collection-section-paragraph'>Discover our latest collection, crafted with modern style and premium comfort. Designed to elevate your everyday look.</p>
                     </div>
                     <div className="items-card-container">
-                        {products.map((product) => (
+                        {latest_products.map((product) => (
                             <ProductCard key={product.id} product={product} />
                         ))}
                     </div>
@@ -63,7 +62,7 @@ const Home = () => {
                         <p className='collection-section-paragraph'>Explore our best sellers, loved by customers for their style, comfort, and quality. These top picks never go out of fashion.</p>
                     </div>
                     <div className="items-card-container">
-                        {best_seller.map((product) => (
+                        {home_best_seller.map((product) => (
                             <BestSellerCard key={product.id} product={product} />
                         ))}
                     </div>
