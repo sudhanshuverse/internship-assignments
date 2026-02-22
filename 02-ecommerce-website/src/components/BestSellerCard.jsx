d68be80e8470e3f9a97bd3ed133f1dedee94b52c
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-02-22 12:06:38 +0530

    Added BestSellerCard.jsx file

--- 02-ecommerce-website/src/components/BestSellerCard.jsx
+++ 02-ecommerce-website/src/components/BestSellerCard.jsx
@@ -1,14 +1,25 @@
-const ProductCard = ({ product }) => {
+const BestSellerCard = ({ product }) => {
     return (
-        <div className="items-card">
-            <div className="card-image-container">
-                <img src={product.image} alt={product.name} />
-            </div>
 
-            <p className="card-title">{product.name}</p>
-            <strong className="card-price">${product.price}</strong>
-        </div>
+        <a
+            href={`/product/${product.id}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="product-link"
+        >
+            <div className="items-card">
+
+                <div className="card-image-container">
+                    <img src={product.image} alt={product.name} />
+                </div>
+
+                <p className="card-title">{product.name}</p>
+                <strong className="card-price">${product.price}</strong>
+
+            </div>
+        </a>
+
     );
 };
 
-export default ProductCard;
+export default BestSellerCard;
